7afc93eba039e786b733a3447c53a759249ca246
Rael <santosrafaelpro@gmail.com>
2024-04-17 21:49:12 -0300

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,14 @@
+print("Verify if a number is prime or not")
+def is_prime(n):
+    if n == 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+num = int(input("Enter a number: "))
+if is_prime(num):
+    print(num, 'is a prime number.')
+else:
+    print(num, 'is not a prime number.')
+4
