8618333634e036b92f707728df7974fb84dc18e5
Rael <santosrafaelpro@gmail.com>
2024-05-14 11:50:49 -0300

    fun py updt

---
+++ Learning/Arg functions.py
@@ -0,0 +1,9 @@
+# ARGUMENTS 
+def happyBirthday(name, age): # remember that the order matters
+    print("Happy Birthday to you!")
+    print(f"You are now {age} years old")
+    print(f"Happy Birthday, dear {name}")
+    print("Happy Birthday to you!\n")
+
+happyBirthday("Rafael", 24) # if you have changed the order in the keyword def or here at the arguments, it will print the wrong output
+happyBirthday("Jonas", 22)
---
+++ Learning/Py Functions.py
@@ -0,0 +1,20 @@
+# functions are block os reusable code
+
+# def keyword is used to define a function
+
+# place () after function name to call the function
+
+def math_topics():
+    print("Addition")
+    print("Subtraction")
+    print("Multiplication")
+    print("Division")
+    
+# calling the function
+
+math_topics()
+math_topics()
+math_topics()
+
+# This will call the function 3 times
+
