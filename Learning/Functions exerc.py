1ae6b8c1d5c8e3265559488dde4ed98729336384
Rael <santosrafaelpro@gmail.com>
2024-05-14 12:09:53 -0300

    Update Functions exerc.py

---
+++ Learning/Functions exerc.py
@@ -0,0 +1,7 @@
+def display_invoice(username, amount, due_date):
+    print(f"Hello {username}")
+    print(f"Amount Due: ${amount} is due on {due_date}")
+    
+    
+display_invoice("Rafael", 24, "2020-12-31")
+    
