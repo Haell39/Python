1ad754fa75b7cf6c2421f0bfdaa26568f182349d
Rael <santosrafaelpro@gmail.com>
2024-05-15 22:45:44 -0300

    update bhask

--- Exercises/odd or even.py
+++ Exercises/odd or even.py
@@ -2,7 +2,7 @@ print("Iniciando programa")
 
 
 while True:
-    a = int(input("Digite um numero: "))
+    a = int(input("Digite um numero, ou digite 0 para encerrar: "))
     if a == 0:
         print("ending program")
         break
