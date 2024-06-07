d3c6122c39142a788f03240fbe0eadf0a44f5d4b
Rael <santosrafaelpro@gmail.com>
2024-06-07 11:26:55 -0300

    Update For Loop.py

--- Advanced I/For Loop.py
+++ Advanced I/For Loop.py
@@ -1,9 +1,29 @@
-print("Loop normal: \n")
+print("\nLoop normal: ")
 
 for x in range(0, 10):
     print(x)
     
-print("Loop com argumento: \n")
+print("\nLoop com argumento: ")
 
-for y in range(0, 21, 3):
+for y in range(0, 22, +3):
     print(y)
+    
+print("\nLoop negativo: ")
+
+for z in range(10, 0, -2):
+    print(z)
+
+print("\nLoop reversed: ")
+
+for i in reversed(range(0, 10)):
+    print(i)
+    
+print("\nLoop reversed com argumento: ")
+
+for j in reversed(range(0, 22, 3)):
+    print(j)
+    
+print("\nLoop reversed negativo: ")
+    
+for rneg in reversed(range(10, 0, -2)):
+    print(rneg)
