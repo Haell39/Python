561943743127845ef21c6f147072af4a962970e2
Rael <santosrafaelpro@gmail.com>
2024-06-13 09:39:49 -0300

    Update new2.py

--- Exercises/new2.py
+++ Exercises/new2.py
@@ -1,6 +1,8 @@
-def numplus(a):
-    return a + 1
+def addition(a, b):
+    sum = a + b
+    return sum
 
-# a = int( input("Type any integuer: "))
-a = 18
-print(numplus(a))
+a = int(input("Type any integuer: "))
+b = int(input("Type any integuer: "))
+
+print(addition(a, b))
