1846f18cec5e6cb409f02eac528f42fd08c74bc9
Rael <santosrafaelpro@gmail.com>
2024-07-04 11:45:44 -0300

    Create main.py

---
+++ Projects/Projeto Bitcoin/main.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from tkinter import ttk
+
+# Colors -------------------
+
+co0 = "#000000" #black
+co1 = "#ffffff" #white
+co2 = "#0000ff" #blue
+
+fundo = "#484f60" 
+
+janela = Tk()
+janela.title('')
+janela.geometry('320x350')
+janela.configure(bg=fundo)
+
+janela.mainloop()
