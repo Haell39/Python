c6657670a7e92d442fa6626a26bfaef8c82f65ff
Rael <santosrafaelpro@gmail.com>
2024-08-04 00:29:06 -0300

    test

--- autoscript.py
+++ autoscript.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 
 x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 y = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+k = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-plt.plot(x, y)
+plt.plot(x, y, k)
 plt.show()
