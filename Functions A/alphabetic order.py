1a6efea279274334e2d54bc9ada015d9d81ea348
Rael <santosrafaelpro@gmail.com>
2024-09-09 10:32:30 -0300

    testes update

--- Functions A/alphabetic order.py
+++ Functions A/alphabetic order.py
@@ -1,8 +1,8 @@
 def ordenar_palavras(frase):
     palavras = frase.split()  # divide a string em uma lista de palavras
-    palavras.sort()  # ordena a lista de palavras em ordem alfabética
+    palavras.sort(key=str.lower)  # ordena a lista de palavras em ordem alfabética
     return ' '.join(palavras)  # junta as palavras novamente em uma string
 
 # Exemplo de uso:
 frase =input("Say your text:")
-print(ordenar_palavras(frase))  
+print(f'Palavras ordenadas: {ordenar_palavras(frase)}')
