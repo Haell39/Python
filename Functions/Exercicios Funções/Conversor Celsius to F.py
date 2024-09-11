e7bc1151dda6767ac2bd8b6f4a19207ec615e50c
Rael <santosrafaelpro@gmail.com>
2024-09-11 00:38:43 -0300

    Update Conversor Celsius to F.py

--- Functions/Exercicios Funções/Conversor Celsius to F.py
+++ Functions/Exercicios Funções/Conversor Celsius to F.py
@@ -1,7 +1,7 @@
 #Conversor de temperatura (Celsius, Fahrenheit) função:
 def celsius_to_fahrenheit(celsius):
   return celsius * 9/5 + 32
-#def fahrenheit_to_celsius(fahrenheit):
+# def fahrenheit_to_celsius(fahrenheit):
   return (fahrenheit - 32) * 5/9
 
 text = int(input("Type a temperature in Celsius: "))
