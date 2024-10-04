383cc0d50b5dadee4a31f140a2d06592101b7a65
Rael <santosrafaelpro@gmail.com>
2024-10-04 10:13:52 -0300

    Update To_Tests.py

--- To_Tests.py
+++ To_Tests.py
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
 def hello():
     msg = "Hello, World!"
     print(msg)
     
 hello()
-=======
-#somar
-def sum(a, b):
-    return a + b
-
-x = sum(1, 2)
-print(x)
->>>>>>> 8148a7f3191e39b65aa84da3814c6dbd8a045c8b
